dfb7b35c103e8771f5eca6cedf1a1caaef834138
AzimovRuslan <55226276+AzimovRuslan@users.noreply.github.com>
2024-05-27 14:53:44 +0300

    fix

--- src/main/java/io/aston/astoncicd/HelloServlet.java
+++ src/main/java/io/aston/astoncicd/HelloServlet.java
@@ -2,27 +2,21 @@ package io.aston.astoncicd;
 
 import java.io.*;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
+    private String message = "Hello everyone!";
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
 
-    public void destroy() {
-    }
 }
